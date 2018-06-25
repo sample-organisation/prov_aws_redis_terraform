a98c8a85a90ce1d00aa3a7ee51910f57c5f06e52
Vishnu Bharathi <vishnubharathi04@gmail.com>
2018-06-25 20:41:22 +0530

    adds redis provisioning code

---
+++ redis.tf
@@ -0,0 +1,30 @@
+provider "aws" {
+  access_key  = "${var.aws_access_key_id}"
+  secret_key  = "${var.aws_secret_access_key}"
+  region      = "${var.aws_region}"
+}
+
+resource "aws_elasticache_cluster" "redisInstance" {
+  cluster_id           = "demo-redis"
+  engine               = "redis"
+  node_type            = "cache.t2.micro"
+  num_cache_nodes      = 1
+  parameter_group_name = "default.redis4.0"
+  port                 = 6379
+}
+
+output "engine_version" {
+  value = "${aws_elasticache_cluster.redisInstance.engine_version}"
+}
+
+output "port" {
+  value = "${aws_elasticache_cluster.redisInstance.port}"
+}
+
+output "cache_node_0_address" {
+  value = "${aws_elasticache_cluster.redisInstance.cache_nodes.0.address}"
+}
+
+output "cache_nodes_0_availability_zone" {
+  value = "${aws_elasticache_cluster.redisInstance.cache_nodes.0.availability_zone}"
+}
---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "aws_access_key_id" {
+  description = "AWS access key"
+}
+
+variable "aws_secret_access_key" {
+  description = "AWS secret access key"
+}
+
+variable "aws_region" {
+  description = "AWS region"
+}
